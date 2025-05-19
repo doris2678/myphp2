e5e03c78a8d92db0bdb05081acbf8b341e13b0a5
doris <dorishsh2678@gmail.com>
2025-05-19 15:29:31 +0800

    include

--- include/header.php
+++ include/header.php
@@ -1,12 +1,14 @@
- <header>
+<?php session_start(); ?>
+<header>
         <h1>學生管理系統</h1>
 </header>
 <nav>
         <ul>
-            <li><a href="index.php">首頁</a></li>
-            <li><a href="list.php">學生列表</a></li>
-            <li><a href="new.php">新增學生</a></li>
-            <li><a href="query.php">查詢學生</a></li>
-            <li><a href="about.php">關於</a></li>
-        </ul>
-</nav>
+            <li><a class="<?=($page=='main')?'active':'';?>" href="index.php">首頁</a></li>
+            <li><a class="<?=($page=='list')?'active':'';?>" href="index.php?page=list">學生列表</a></li>
+            <li><a class="<?=($page=='new')?'active':'';?>" href="index.php?page=new">新增學生</a></li>
+            <li><a class="<?=($page=='query')?'active':'';?>" href="index.php?page=query">查詢學生</a></li>
+            <li><a class="<?=($page=='about')?'active':'';?>" href="index.php?page=about">關於</a></li>
+        </ul> 
+</nav> 
+ 
